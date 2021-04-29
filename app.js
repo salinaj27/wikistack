94962ef6f7d2ca39efb0859b07357a5677046b84
Adam Weinberg <adamcweinberg@gmail.com>
2021-04-29 13:16:47 -0700

    synced database tables

--- app.js
+++ app.js
@@ -1,15 +1,26 @@
 const ex = require("express");
 const morg = require("morgan");
+const index = require('./views/index')
+const {db} = require('./models')
+
 const app = ex();
+
 app.use(morg("dev"));
 app.use(ex.static(__dirname + "/public"));
 app.use(ex.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => {
-  res.send("WORK");
+  res.send(index.main());
 });
 
 const PORT = 3000;
-app.listen(PORT, () => {
-  console.log("LISTEN");
-});
+
+async function syncDb() {
+  await db.sync( {force: true} )
+
+  app.listen(PORT, () => {
+    console.log("LISTEN");
+  })
+}
+
+syncDb()
